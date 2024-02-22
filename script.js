02e96484193f105ccfd2f7e0c38659434213069c
Rooneyish <ilustraterp@gmail.com>
2024-02-22 08:07:28 +0545

    third commit
    added the functions that displays number onclick on buttons on display bar

--- script.js
+++ script.js
@@ -34,10 +34,127 @@ function operate(operand,firstNum,secondNum){
     }
 }
 
-function seven(7){
-    let seven=document.querySelector("#7");
-    let display=document.querySelector(".display")
-    seven.addEventListener("clicl", (displays)=>{
-        display.innerHtml="<p>7</p>"
-    })
+
+function seven(){
+    let seven=document.querySelector('#seven');
+    let displays=document.querySelector('.display');
+    seven.addEventListener("click", function(){
+        displays.innerHTML=7;
+    }
+    )
+}
+
+function eight() {
+  let seven = document.querySelector("#eight");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 8;
+  });
+}
+
+function nine() {
+  let seven = document.querySelector("#nine");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 9;
+  });
+}
+
+function plus() {
+  let seven = document.querySelector("#plus");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = "+";
+  });
+}
+function four() {
+  let seven = document.querySelector("#four");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 4;
+  });
+}
+function five() {
+  let seven = document.querySelector("#five");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 5;
+  });
+}
+function six() {
+  let seven = document.querySelector("#six");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 6;
+  });
+}
+function minus() {
+  let seven = document.querySelector("#minus");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = '-';
+  });
+}
+function one() {
+  let seven = document.querySelector("#one");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 1;
+  });
+}
+function two() {
+  let seven = document.querySelector("#two");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 2;
+  });
+}
+function three() {
+  let seven = document.querySelector("#three");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 3;
+  });
+}
+function star() {
+  let seven = document.querySelector("#star");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = '*';
+  });
+}
+function zero() {
+  let seven = document.querySelector("#zero");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 0;
+  });
+}
+function clear() {
+  let seven = document.querySelector("#clear");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = "";
+  });
+}
+function equal() {
+  let seven = document.querySelector("#equal");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = '=';
+  });
+}
+function div() {
+  let seven = document.querySelector("#div");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = '/';
+  });
+}
+function eight() {
+  let seven = document.querySelector("#eight");
+  let displays = document.querySelector(".display");
+  seven.addEventListener("click", function () {
+    displays.innerHTML = 8;
+  });
 }
